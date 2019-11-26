de8e39286a4b9d1082dc0aee75f9e436a39e4464
nicola.sansonetti <nicola.sansonetti@finconsgroup.com>
2019-11-26 22:28:00 +0100

    project setup

---
+++ main.js
@@ -0,0 +1,91 @@
+const handleParse = () => {
+    let dump = document.getElementById('dump').value;
+    let template = document.getElementById('ofx').value;
+    let output = document.getElementById('output');
+
+    let data = getCUrlData(getCUrl(dump));
+
+    output.value = unescape(replaceTemplate(template, data));
+}
+
+const replaceTemplate = (src, data) => {
+    let doc = parseXmlFromString(src);
+
+    data.forEach(d => {
+        const query = `[select*="@name='${d.field}'"]`;
+        doc.querySelectorAll(query).forEach(el => {
+            let parent = el.parentNode;
+            el.remove();
+            parent.textContent = d.value;
+        });
+    });
+
+    return new XMLSerializer().serializeToString(doc);
+}
+
+const getCUrl = (src) => {
+    let doc = parseXmlFromString(src);
+    let nodes = getDumpNodes(doc);
+    let START_CURL_INDEX = indexOfStringArray(nodes, 'START CURL');
+    let END_CURL_INDEX = indexOfStringArray(nodes, 'END CURL');
+
+    if (START_CURL_INDEX != -1 && END_CURL_INDEX != -1) {
+        return nodes.slice(START_CURL_INDEX, END_CURL_INDEX+1).join("")
+    } else {
+        throw new Error("cURL request not found.");
+    }
+}
+
+/**
+ * Estrae il valore del parametro -d corrispondente ai dati della cURL
+ * @param {string} cURL 
+ */
+const getCUrlData = (cURL) => {
+    let temp = cURL.slice(cURL.indexOf('-d'));
+    temp = temp.slice(temp.indexOf('"') + 1);
+    temp = temp.slice(0, temp.indexOf('"'));
+    return temp.split('&').map(el => ( { field: el.split('=')[0], value: el.split('=')[1] } ));
+}
+
+/**
+ * Converte una stringa contenente XML in un oggetto di tipo XMLDocument.
+ * @param {string} text 
+ */
+const parseXmlFromString = (text) => {
+    let parser = new DOMParser();
+    let doc = parser.parseFromString(text, "application/xml");
+    removeElement(doc, 'parsererror');
+    return doc;
+}
+
+const removeElement = (dom, query) => {
+    dom.querySelectorAll(query).forEach(el => el.remove());
+}
+
+/**
+ * Estrae il valore dell'attributo "node" per ogni nodo "nodeinfo" trovato.
+ * @param {XMLDocument} doc 
+ */
+const getDumpNodes = (doc) => {
+    let xPathResult = document.evaluate(".//nodeinfo/@node", doc, null, XPathResult.ANY_TYPE, null);
+    let nodes = [];
+    let node = xPathResult.iterateNext();
+    while (node) {
+        nodes.push(node);
+        node = xPathResult.iterateNext();
+    }
+    return nodes.map(el => el.value);
+}
+
+/**
+ * Ritorna l'indice del primo array contenente la stringa passata in input.
+ * @param {Array} arr 
+ * @param {string} text 
+ */
+const indexOfStringArray = (arr, text) => {
+    let result = null;
+    if (arr && arr.length) {
+        result = arr.reduce((acc, cur, idx) => cur.indexOf(text) != -1 ? acc = idx : acc);
+    }
+    return result ? +result : -1;
+}
